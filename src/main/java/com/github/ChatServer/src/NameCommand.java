67e7ae0110e3db2a24c2d7be76dd7c0e9f739b13
上野山雄太 <yu.uver.199469@gmail.com>
2015-07-28 21:14:31 +0900

    refs #5 NAMEコマンドを実装
    NAMEコマンドを実装しました。

--- src/main/java/com/github/ChatServer/src/NameCommand.java
+++ src/main/java/com/github/ChatServer/src/NameCommand.java
@@ -11,6 +11,19 @@ public class NameCommand implements CommandInterFace {
 	}
 	@Override
 	public void actionCommand() {
-	
+        // TODO 自動生成されたメソッド・スタブ
+		UserManager manager = UserManager.getInstance();
+		if(manager.serchUserName(name) == null){
+			this.handler.userdata.setName(this.name);
+		}else{
+			try {
+				handler.out.write("Error-既に名前が使われています");
+				handler.out.write("\r\n");
+				handler.out.flush();
+			} catch (IOException e) {
+				// TODO: handle exception
+				System.out.println(e);
+			}
+		}
 	}
 }
