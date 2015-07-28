33f6873d4236d29f76893e37152b53306d19d82c
曽和修平 <sowashuhei@gmail.com>
2015-07-28 20:46:12 +0900

    ヘルプコマンドの実装

--- src/main/java/com/github/ChatServer/src/HelpCommand.java
+++ src/main/java/com/github/ChatServer/src/HelpCommand.java
@@ -10,7 +10,21 @@ public class HelpCommand implements CommandInterFace {
 
 	@Override
 	public void actionCommand() {
-
+		try {
+		handler.out.write(
+				"hrlp - 処理可能な命令一覧を表示\n"
+				+ "name - 名前を設定\n"
+				+ "whoami - 現在設定されてる名前を表示\n"
+				+ "users - チャットに参加しているメンバの名前を表示\n"
+				+ "bye - チャットを終了\n"
+				+ "post - 全員にメッセージを送信\n"
+				+ "tell - 指定された名前のメンバにのみメッセージを送信\n");
+		handler.out.write("\r\n");
+		handler.out.flush();
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
 	
 	}
 
